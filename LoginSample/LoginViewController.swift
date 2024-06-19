8e87a0e2d41ecac39493e94b49554f55ba7b0211
ychic1109-DA <ychic1109@gmail.com>
2024-06-19 13:50:07 +0900

    Initial Commit

---
+++ LoginSample/LoginViewController.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var idTextField: UITextView!
+    @IBOutlet weak var pwTextField: UITextView!
+    
+    // 값 확인용 임시 Label
+    @IBOutlet weak var pwTest: UILabel!
+    @IBOutlet weak var idTest: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLayout()
+        
+        
+    }
+    
+    let id: String = "Admin"
+    let password:String = "1004"
+    
+    func setLayout() {
+//        idTextField.
+    }
+    
+    @IBAction func checkBtn(_ sender: Any) {
+        
+        idTest.text = idTextField.text
+        pwTest.text = pwTextField.text
+        
+//        UITextFieldDelegate()
+        
+        if (idTextField.text == self.id) && (pwTextField.text == self.password) {
+            print("로그인에 성공하셨습니다 :)")
+            idTest.backgroundColor = .green}
+            
+        else {
+            print("올바른 아이디와 비밀번호를 입력해 주세요!")
+            idTest.backgroundColor = .red
+            
+            
+            
+            
+            // or 삼항연산자 사용
+            //        let result = (idTextField.text == self.id) && (pwTextField.text == self.password)
+            //        ? print("로그인에 성공하셨습니다 :)") : print("올바른 아이디와 비밀번호를 입력해 주세요!")
+            
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+}
+
---
+++ LoginSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginSample
+//
+//  Created by t2023-m0013 on 6/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
