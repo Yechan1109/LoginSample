13379622ceada4ca1f92822cb728896a7dc041f3
ychic1109-DA <gamdev_93@naver.com>
2024-06-21 11:14:42 +0900

    - init

--- LoginSample/ViewController.swift
+++ LoginSample/ViewController.swift
@@ -9,12 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var loginTextField: UITextField = {
+       let textField = UITextField()
+        
+       return textField
+    }()
+    
+    private let loginInfoLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+        let textField = UITextField()
+        
+        return textField
+    }()
+    
+    private let passwordInfoLabel: UILabel = {
+       let label = UILabel()
+        
+       return label
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-//asdasdasd
-    var test = ""
 
+    
+    
 }
 
+
+extension ViewController: UITextFieldDelegate {
+    
+}
