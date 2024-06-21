56b8f8c136081d3345298e34c9ddca8f997930a4
ychic1109-DA <gamdev_93@naver.com>
2024-06-21 19:28:29 +0900

    signUpManager add

---
+++ LoginSample/Model/SignUpManager.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpManager.swift
+//  LoginSample
+//
+//  Created by 남지연 on 6/21/24.
+//
+
+import Foundation
+
+class SignUpManager {
+    
+    static let shared = SignUpManager()
+    
+    private init() {}
+    
+}
