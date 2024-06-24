2e5d7453cd0b7ba442f0388013b0859d5753d2fb
ychic1109-DA <ychic1109@gmail.com>
2024-06-24 20:25:51 +0900

    no message

--- LoginSample/Controller/LoginViewController.swift
+++ LoginSample/Controller/LoginViewController.swift
@@ -36,48 +36,150 @@ final class ViewController: UIViewController {
     
     private lazy var idTextField: UITextField = {
         let textField = UITextField()
-        textField.frame.size.width = 100
-        textField.frame.size.height = 50
-        textField.placeholder = "아이디를 입력해 주세요"
-        
+        textField.backgroundColor = .black
+        textField.font = UIFont.systemFont(ofSize: 15)
+        textField.textColor = .white
+        textField.attributedPlaceholder = NSAttributedString(string: "아이디를 입력해 주세요", attributes: [.foregroundColor: UIColor.systemGray])
+        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
+    
+    private lazy var passwordInfoLabel: UILabel = {
+       let label = UILabel()
+        label.backgroundColor = .black
+        label.text = "Password"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 15)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+       return label
+    }()
+    
+    private lazy var passwordTextField: UITextField = {
+            let textField = UITextField()
+            textField.backgroundColor = .black
+            textField.textColor = .white
+            textField.font = UIFont.systemFont(ofSize: 15)
+            textField.tintColor = .white
+            textField.attributedPlaceholder = NSAttributedString(string: "비밀번호를 입력해 주세요", attributes: [.foregroundColor: UIColor.systemGray])
+            textField.autocapitalizationType = .none
+            textField.autocorrectionType = .no
+            textField.spellCheckingType = .no
+            textField.isSecureTextEntry = true
+            textField.clearsOnBeginEditing = false
+            //textField.addTarget(self, action: #selector(<#T##@objc method#>), for: .touchUpInside)
+            textField.translatesAutoresizingMaskIntoConstraints = false
+            return textField
+        }()
+    
+    private lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(hexCode: "#FFBF00")
+        button.layer.cornerRadius = 10
+        button.setTitle("Login", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.isEnabled = false
+
+//        button.addTarget(self, action: #selector(loginButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var passwordResetButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor(hexCode: "#960018")
+        button.setTitle("password 다시", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        //button.addTarget(self, action: #selector(<#T##@objc method#>), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    private lazy var vStackView: UIStackView = {
+            let stackView = UIStackView()
+            stackView.axis = .vertical
+            stackView.spacing = 10
+            stackView.distribution = .fillEqually
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         configure()
+        makeUI()
+        
 
     }
-
-    
-    
         
-        
+    
+    private func makeUI() {
+        NSLayoutConstraint.activate([
+            
+            vStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            vStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            vStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            vStackView.heightAnchor.constraint(equalToConstant: 250),
+            
+//            passwordResetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+//            passwordResetButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20)
+        ])
+    }
+    
     private func configure() {
         view.backgroundColor = .darkGray
+        view.addSubview(vStackView)
         
-//        view.addSubview(idView)
-        view.addSubview(idInfoLabel)
+        vStackView.addArrangedSubview(idInfoLabel)
+        vStackView.addArrangedSubview(idTextField)
+        vStackView.addArrangedSubview(passwordInfoLabel)
+        vStackView.addArrangedSubview(passwordTextField)
+        vStackView.addArrangedSubview(loginButton)
+        }
+
         
-        NSLayoutConstraint.activate([
-//            idView.widthAnchor.constraint(equalToConstant: 300),
-//            idView.widthAnchor.constraint(equalToConstant: 300),
-//            idView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-//            idView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            
-            idInfoLabel.widthAnchor.constraint(equalToConstant: 100),
-            idInfoLabel.heightAnchor.constraint(equalToConstant: 50),
-            idInfoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-//            idInfoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            idInfoLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        
-        ])
-        
-        
-        
-      
-    }
+//    private func configure() {
+//        view.backgroundColor = .darkGray
+//        
+////        view.addSubview(idView)
+////        view.addSubview(idInfoLabel)
+//        
+//        [idInfoLabel, idTextField, passwordInfoLabel, passwordTextField].forEach { view.addSubview($0)
+////            NSLayoutConstraint.activate([
+////                $0.widthAnchor.constraint(equalTo: 300)
+////                $0.heightAnchor.constraint(equalTo: 40)
+////            ])
+//        }
+//        
+//        NSLayoutConstraint.activate([
+//
+//            idInfoLabel.widthAnchor.constraint(equalToConstant: 300),
+//            idInfoLabel.heightAnchor.constraint(equalToConstant: 40),
+//            idInfoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+//            idInfoLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+//                    
+//            idTextField.widthAnchor.constraint(equalToConstant: 300),
+//            idTextField.heightAnchor.constraint(equalToConstant: 40),
+//            idTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+//            idTextField.topAnchor.constraint(equalTo: idInfoLabel.bottomAnchor, constant: 10),
+//            
+//            passwordInfoLabel.widthAnchor.constraint(equalToConstant: 300),
+//            passwordInfoLabel.heightAnchor.constraint(equalToConstant: 40),
+//            passwordInfoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+//            passwordInfoLabel.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: 10),
+//            
+//            passwordTextField.widthAnchor.constraint(equalToConstant: 300),
+//            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+//            passwordTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+//            passwordTextField.topAnchor.constraint(equalTo: passwordInfoLabel.bottomAnchor, constant: 10),
+//        ])
+//        
+//    }
     
     //⭐️그 외 로그인 방식⭐️
     //카카오, 구글, 애플 구현 예정
@@ -92,27 +194,48 @@ final class ViewController: UIViewController {
 }
 
 
-//extension ViewController: UITextFieldDelegate {
-//    
-//    // MARK: - 텍스트 필드 편집 시작할 때 설정 (문구가 올라가면서 크기 작아짐)
-//    func textFieldDidBeginEditing(_ textField: UITextField) {    //autoLayout Update
-//        
-//    }
-//    
-//    // MARK: - 텍스트 필드 종료 시점 (글자 / 백그라운드 update)
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        
-//    }
-//    
-//    // MARK: - id / pw 로그인 버튼 색 변경
-//    @objc private func textFieldEditingCharge(_ textField: UITextField) {
-//        
-//    }
-//    
-//    // MARK: - Enter 다음 줄로 이동 및 키보드 활성 유무
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        return true
-//    }
-//
-//
-//}
+extension ViewController: UITextFieldDelegate {
+    
+    // MARK: - 텍스트 필드 편집 시작할 때 설정 (문구가 올라가면서 크기 작아짐)
+    func textFieldDidBeginEditing(_ textField: UITextField) {    //autoLayout Update
+        
+    }
+    
+    // MARK: - 텍스트 필드 종료 시점 (글자 / 백그라운드 update)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    
+    // MARK: - id / pw 로그인 버튼 색 변경
+    @objc private func textFieldEditingCharge(_ textField: UITextField) {
+        
+    }
+    
+    // MARK: - Enter 다음 줄로 이동 및 키보드 활성 유무
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true
+    }
+
+
+}
+
+extension UIColor {
+    
+    convenience init(hexCode: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+        
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+        
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+}
